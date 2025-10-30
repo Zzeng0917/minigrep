84edf5bd4aa8dd7ecfa8d8d217d54b1f38936869
XianDe-Zeng <156569749+Zzeng0917@users.noreply.github.com>
2025-10-30 22:42:39 +0800

    Change println to eprintln for error messages

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     // 对 build 返回的 `Result` 进行处理
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);//终结进程，因为错误而退出
     });
 
@@ -17,7 +17,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
